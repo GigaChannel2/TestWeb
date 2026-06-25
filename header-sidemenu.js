b4a7ad85e32e2e88ee302009a27e6996f16f2f38
Giga Channel <gigachannel115@gmail.com>
2026-06-25 16:41:48 +0700

    IDEK

--- header-sidemenu.js
+++ header-sidemenu.js
@@ -1,64 +1,71 @@
 
-let header = document.getElementById("main-header");
-let header_logo = document.getElementById("logo");
+// document.addEventListener("headerLoaded", ()=>{
 
-let sidemenu = document.getElementById("sidemenu");
-let sidemenu_btn = sidemenu.querySelectorAll("a");
+    let header = document.getElementById("main-header");
+    let header_logo = document.getElementById("logo");
 
-let backmenu = document.getElementById("backmenu");
-let backshadow = document.getElementById("backshadow");
+    let sidemenu = document.getElementById("sidemenu");
+    let sidemenu_btn = sidemenu.querySelectorAll("a");
 
-let isMenu = false;
+    let backmenu = document.getElementById("backmenu");
+    let backshadow = document.getElementById("backshadow");
 
-let ayam = document.getElementById("ayam");
-let ayam2 = document.getElementById("ayam2");
+    let ayam = document.getElementById("ayam");
+    let ayam2 = document.getElementById("ayam2");
 
-ayam.addEventListener("click", (e)=>{
-    e.preventDefault();
-    new Audio("sfx/ayam.mp3").play();
-});
+    let isMenu = false;
 
-ayam2.addEventListener("click", (e)=>{
-    e.preventDefault();
-    new Audio("sfx/ayam.mp3").play();
-});
-
-function open_menu() {
-    if (!isMenu) {
-        isMenu = true;
-        backshadow.style.visibility = "visible";
-        sidemenu.style.left = 0;
-        backshadow.style.opacity = 0.4;
-    }
-}
-
-function close_menu() {
-    if (isMenu) {
-        isMenu = false;
-        sidemenu.style.left = "-700px";
-        backshadow.style.opacity = 0;
-        setTimeout(()=>{
-            backshadow.style.visibility = "hidden";
-        }, 500)
-    }
-}
+    console.log(sidemenu);
 
 
-sidemenu_btn.forEach(btn => {
-    btn.addEventListener("click", (e) => {
+    ayam.addEventListener("click", (e)=>{
         e.preventDefault();
-    })
-    // btn.addEventListener("mouseenter", (e) => {
-
-    // })
-});
-
-sidebar_btn.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        // history.pushState({}, "", "/profile");
-
+        new Audio("sfx/ayam.mp3").play();
     });
-});
 
+    ayam2.addEventListener("click", (e)=>{
+        e.preventDefault();
+        new Audio("sfx/ayam.mp3").play();
+    });
+
+    function open_menu() {
+        if (!isMenu) {
+            isMenu = true;
+            backshadow.style.visibility = "visible";
+            sidemenu.style.left = 0;
+            backshadow.style.opacity = 0.4;
+        }
+    }
+
+    function close_menu() {
+        if (isMenu) {
+            isMenu = false;
+            sidemenu.style.left = "-700px";
+            backshadow.style.opacity = 0;
+            setTimeout(()=>{
+                backshadow.style.visibility = "hidden";
+            }, 500)
+        }
+    }
+
+
+    sidemenu_btn.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            e.preventDefault();
+        })
+        // btn.addEventListener("mouseenter", (e) => {
+
+        // })
+    });
+
+    // sidebar_btn.forEach(btn => {
+    //     btn.addEventListener("click", (e) => {
+    //         e.preventDefault();
+
+    //         // history.pushState({}, "", "/profile");
+
+    //     });
+    // });
+
+
+// });
---
+++ post-system.js
@@ -0,0 +1,11 @@
+const slug = new URL(location)
+.searchParams
+.get("p");
+
+// if(slug){
+
+//     const md = await fetch(
+//         `posts/${slug}.md`
+//     ).then(r=>r.text());
+
+// }
