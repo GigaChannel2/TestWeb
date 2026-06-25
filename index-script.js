53ad0dcb0de29ec852a328a7c3fe5bc74b157368
Giga Channel <gigachannel115@gmail.com>
2026-06-25 15:29:41 +0700

    removed sidebar & added post page (empty)

--- index-script.js
+++ index-script.js
@@ -1,18 +1,10 @@
 const debug = true;
 
-let header = document.getElementById("main-header");
-let header_logo = document.getElementById("logo");
-let ayam = document.getElementById("ayam");
-let ayam2 = document.getElementById("ayam2");
-let sidebar = document.getElementById("sidebar");
-let sidebar_btn = sidebar.querySelectorAll("a");
 let page = document.getElementById("page");
 let page_txt = page.querySelector("h1");
-let sidemenu = document.getElementById("sidemenu");
-let sidemenu_btn = sidemenu.querySelectorAll("a");
-let backmenu = document.getElementById("backmenu");
-let backshadow = document.getElementById("backshadow");
-let isMenu = false;
+let sidebar = document.getElementById("sidebar");
+let sidebar_btn = sidebar.querySelectorAll("a");
+
 let isNotFound = Boolean;
 
 if (isNotFound) {
@@ -32,33 +24,6 @@ const routes = {
     "/TestWeb/bio": "This is a bio<br> yes, its a bio<br> yes, it is a bio."
 }
 
-ayam.addEventListener("click", (e)=>{
-    e.preventDefault();
-    new Audio("sfx/ayam.mp3").play();
-});
-
-ayam2.addEventListener("click", (e)=>{
-    e.preventDefault();
-    new Audio("sfx/ayam.mp3").play();
-});
-
-sidemenu_btn.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-    })
-    // btn.addEventListener("mouseenter", (e) => {
-
-    // })
-});
-
-sidebar_btn.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        // history.pushState({}, "", "/profile");
-
-    });
-});
 
 // Handle browser navigation (back/forward buttons)
 window.onpopstate = updateContent;
@@ -76,26 +41,6 @@ if (debug) {
         console.log(isMenu);
         
     }, 600)
-}
-
-function open_menu() {
-    if (!isMenu) {
-        isMenu = true;
-        backshadow.style.visibility = "visible";
-        sidemenu.style.left = 0;
-        backshadow.style.opacity = 0.4;
-    }
-}
-
-function close_menu() {
-    if (isMenu) {
-        isMenu = false;
-        sidemenu.style.left = "-400px";
-        backshadow.style.opacity = 0;
-        setTimeout(()=>{
-            backshadow.style.visibility = "hidden";
-        }, 500)
-    }
 }
 
 function pagech(path) {
